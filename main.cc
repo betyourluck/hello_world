e14bde4c69276fe40c88a10520601f0138ac72a7
conan edogawa <betyourluck@users.noreply.github.com>
2017-05-17 10:16:29 +0900

    Create main.cc
    exsample

---
+++ main.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int
+main(int argc, char *argv[])
+{
+  std::vector<string> v = {"Hello","Good morning","Good Afternoon"};
+  std::replace_if(v.begin(),v.end(),[](const std::string& s){
+      return (s == "Good morning" || s == "Good Afternoon");
+  },"Hello");
+
+  std::for_each(v.begin(),v.end(),[](const std::string& s){
+      std::cout << s << " World!"<< std::endl;});
+
+  return 0;
+}
